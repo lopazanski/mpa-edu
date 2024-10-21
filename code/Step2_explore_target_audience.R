4ee62979cd33e272d52c34a0db14fa461a1bee1c
Alexa Gray <alexa.gray034@gmail.com>
2024-10-21 13:32:41 -0700

    update target audience

--- code/Step2_explore_target_audience.R
+++ code/Step2_explore_target_audience.R
@@ -14,19 +14,14 @@ orig_list <- readRDS("data/raw-ish/education_export_21Oct2024.Rds")
 target <- orig_list %>% 
   distinct(obs_id, plan_id, edu_target_audience) %>% 
   separate(edu_target_audience, into = paste0("part_", 1:8), sep = ";", fill = "right", extra = "merge") %>%
+  mutate(across(everything(), str_trim)) %>% 
+  pivot_longer(part_1:part_8, values_to = "edu_target_audience", names_to = NULL, values_drop_na = T) %>% 
   group_by(edu_target_audience) %>% 
   summarize(n_strategies = length(unique(obs_id)),
             n_plans = length(unique(plan_id))) %>% 
   mutate(percent_of_strategies = n_strategies/length(unique(orig_list$obs_id))*100,
          percent_of_plans = n_plans/length(unique(orig_list$plan_id))*100)
 
-# Save ------------------------------------------------------------------------
+# Share ------------------------------------------------------------------------
 
-# Save this
-
-
-# here 
-
-something else
-  
-  
+saveRDS(target, "data/processed/target-audience.Rds")
