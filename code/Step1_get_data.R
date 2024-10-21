0b06deb61627b001e1741344a8f8607cde0e0570
lopazanski <cori.lopazanski@gmail.com>
2024-10-21 13:20:16 -0700

    reorganize and re-export edu data

---
+++ code/Step1_get_data.R
@@ -0,0 +1,23 @@
+# Pull education data from google drive
+# Cori Lopazanski
+# October 2024
+
+# Setup ------------------------------------------------------------------------
+library(tidyverse)
+library(googlesheets4)
+library(janitor)
+library(googledrive)
+
+
+drive.dir <- "/Users/lopazanski/Library/CloudStorage/GoogleDrive-lopazanski@ucsb.edu/Shared drives/4-MPA Education/2-Data"
+
+
+# Read data from google --------------------------------------------------------
+
+orig_list <- read_sheet("https://docs.google.com/spreadsheets/d/1pA9tHg_jjx9XXXxRRws8KiTv0P5nsXWR-1Oz5JyLL1o/edit?gid=1188768943#gid=1188768943",
+                        sheet = "USE THIS plan-info", trim_ws = T, range = "A:Q",
+                        na = c("NA", "", "N/A"), col_types = "ncnccccccncncncnc") %>% 
+  mutate(obs_id = rownames(.)) %>% 
+  clean_names()
+
+saveRDS(orig_list, "data/raw-ish/education_export_21Oct2024.Rds")
